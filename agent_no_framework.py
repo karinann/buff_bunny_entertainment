a294df10e9b309fe0834dd8c475367854558ab63
Shanelle <shanellehan2287@gmail.com>
2026-07-11 20:20:34 -0400

    Reverted back to last kind of working code

--- agent_no_framework.py
+++ agent_no_framework.py
@@ -1,10 +1,19 @@
 from google import genai
 from google.genai import types
 from pydantic import BaseModel
+from typing import Optional
 import json 
+
 
 class PlanSteps(BaseModel):
     step_name: str
+
+class ExecuteTask(BaseModel):
+    task_name: str
+    action_performed: str
+    summary: str
+    simulated_data: Optional[str] = None
+
 
 def callmodel(prompt, schema):
     response = chat.send_message(prompt, config = types.GenerateContentConfig(
@@ -24,14 +33,28 @@ def plan_goal():
     steps = [ plan['step_name'].strip() for plan in plans if plan['step_name'].strip() ]
     return steps
 
+def execute_step(step):
+    print("---------------------")
+    action_prompt = f"Execute this task : {step}. Describe what you did and summarize the result. Simulate the steps if required"
+    result = callmodel(action_prompt, ExecuteTask)
+    result = json.loads(result)
+
+    print("Task: ", result['task_name'])
+    print("Action: ", result['action_performed'])
+    print("Summary: ", result['summary'])
+
+    if result['simulated_data']:
+        print("Data: ", result['simulated_data'].strip())
+    print("---------------------")
+
+
+
 def run_agent():
     steps = plan_goal()
     for step in steps:
-        print(step)
-
-
-        #execute_step(step)
-    print(" \n compled step \n ")
+        #print(step)
+        execute_step(step)
+    print(" \n completed step \n ")
 
 if __name__ == "__main__":
     client = genai.Client()
